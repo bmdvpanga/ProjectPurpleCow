413d06ab85fa851237feb5f65e6700981a1c314d
Ben <bmdvpanga@gmail.com>
2021-03-12 08:52:33 -0500

    port as param...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        port(3000);
+        //grab port as a parameter
+        int port = args.length == 0 ? 3000 : Integer.parseInt(args[0]);
+//        //set up port
+        port(port);
         new ItemController(new ItemService());
 
     }
